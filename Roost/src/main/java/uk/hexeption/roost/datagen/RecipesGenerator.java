fce758626355967992fdfc4a4e94818368e5423e
Hexeption <minecraftfun201@gmail.com>
2020-09-17 15:38:43 +0100

    Added Crafting recipe for Roost block

--- Roost/src/main/java/uk/hexeption/roost/datagen/RecipesGenerator.java
+++ Roost/src/main/java/uk/hexeption/roost/datagen/RecipesGenerator.java
@@ -2,11 +2,14 @@ package uk.hexeption.roost.datagen;
 
 import java.util.function.Consumer;
 import net.minecraft.advancements.criterion.InventoryChangeTrigger;
+import net.minecraft.advancements.criterion.ItemPredicate;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.IFinishedRecipe;
 import net.minecraft.data.RecipeProvider;
 import net.minecraft.data.ShapedRecipeBuilder;
 import net.minecraft.item.Items;
+import net.minecraft.tags.ItemTags;
+import uk.hexeption.roost.setup.ModBlocks;
 import uk.hexeption.roost.setup.ModItems;
 
 /**
@@ -34,6 +37,16 @@ public class RecipesGenerator extends RecipeProvider {
             .addCriterion("egg", InventoryChangeTrigger.Instance.forItems(Items.EGG))
             .addCriterion("stick", InventoryChangeTrigger.Instance.forItems(Items.STICK))
             .addCriterion("feather", InventoryChangeTrigger.Instance.forItems(Items.FEATHER))
+            .build(consumer);
+
+        ShapedRecipeBuilder.shapedRecipe(ModBlocks.ROOST.get())
+            .patternLine("WWW")
+            .patternLine("W W")
+            .patternLine("HHH")
+            .key('W', ItemTags.PLANKS)
+            .key('H', Items.HAY_BLOCK)
+            .addCriterion("planks", InventoryChangeTrigger.Instance.forItems(ItemPredicate.Builder.create().tag(ItemTags.PLANKS).build()))
+            .addCriterion("hay", InventoryChangeTrigger.Instance.forItems(Items.HAY_BLOCK))
             .build(consumer);
     }
 }
